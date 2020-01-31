270c4d643bde5dd4b74324cb3f8617f636abcb5b
Luiz Silva <39914226+luiz117@users.noreply.github.com>
2020-01-30 21:54:56 -0300

    Add files via upload

--- for/src/App.java
+++ for/src/App.java
@@ -6,43 +6,46 @@ public class App {
 		
 		//Random r = new Random();
 		
-		int w,x,y,z;
-		int a,b,c;		
-		int matrix[][][] = new int [7][8][9];
-		int min=1;
-		int max=9999;
-		matrix m = new matrix();
-		for(w=0;w<matrix.length ;w++) {
-			c=m.geraNum(min, max);
-			//c = r.nextInt((9000 - 1000) + 1) + 1000;
-			matrix[w][0][0]=c;			
-			for(x=0;x<matrix[w].length;x++) {
-				b=m.geraNum(min, max);
-				//b = r.nextInt((9000 - 1000) + 1) + 1000;
-				matrix[w][x][0]=b;
-				for(y=0;y<matrix[w][x].length;y++) {
-					a=m.geraNum(min, max);
-					//a = r.nextInt((9000 - 1000) + 1) + 1000;
-					matrix[w][x][y]=a;
-				}				
-			}			
-		}
+//		int w,x,y,z;
+//		int a,b,c;		
+//		int matrix[][][] = new int [7][8][9];
+//		int min=1;
+//		int max=9999;
+//		matrix m = new matrix();
+//		for(w=0;w<matrix.length ;w++) {
+//			c=m.geraNum(min, max);
+//			//c = r.nextInt((9000 - 1000) + 1) + 1000;
+//			matrix[w][0][0]=c;			
+//			for(x=0;x<matrix[w].length;x++) {
+//				b=m.geraNum(min, max);
+//				//b = r.nextInt((9000 - 1000) + 1) + 1000;
+//				matrix[w][x][0]=b;
+//				for(y=0;y<matrix[w][x].length;y++) {
+//					a=m.geraNum(min, max);
+//					//a = r.nextInt((9000 - 1000) + 1) + 1000;
+//					matrix[w][x][y]=a;
+//				}				
+//			}			
+//		}
+//		
+//		
+//		
+//		for(w=0;w<matrix.length ;w++) {							
+//			for(x=0;x<matrix[0].length;x++) {				
+//				for(y=0;y<matrix[0][0].length ;y++) {					
+//					System.out.print(matrix[w][x][y] + " ");
+//				}
+//				
+//				System.out.print("\t \t");
+//			}	
+//			
+//			System.out.println("\t");
+//		}
+//		
 		
-		
-		
-		for(w=0;w<matrix.length ;w++) {							
-			for(x=0;x<matrix[0].length;x++) {				
-				for(y=0;y<matrix[0][0].length ;y++) {					
-					System.out.print(matrix[w][x][y] + " ");
-				}
-				
-				System.out.print("\t \t");
-			}	
-			
-			System.out.println("\t");
-		}
-		
-
+		matrix m = new matrix(-11111,99999);
+		//m.geraMatriz(7, 5, 3);		
+		m.showMatrix(m.geraMatriz(5, 2, 7));
 
 	}
 
--- for/src/matrix.java
+++ for/src/matrix.java
@@ -1,13 +1,95 @@
 import java.util.Random;
 
-public class matrix {
+public class matrix {	
+	
+	private int min;
+	private int max;
+	
+	public matrix(int min, int max) {
+		this.min=min;
+		this.max=max;
+	}
 	
 	
-	public int geraNum(int min, int max) {	
+	
+	/**
+	 * @return the min
+	 */
+	public int getMin() {
+		return min;
+	}
+
+
+	/**
+	 * @param min the min to set
+	 */
+	public void setMin(int min) {
+		this.min = min;
+	}
+
+
+	/**
+	 * @return the max
+	 */
+	public int getMax() {
+		return max;
+	}
+
+
+	/**
+	 * @param max the max to set
+	 */
+	public void setMax(int max) {
+		this.max = max;
+	}
+
+
+	public static int geraNum(int min, int max) {		
 		Random random = new Random();
 		int a = random.nextInt((max - min) + 1) + min;
 		return a;
 	}
+	
+	
+	//L = LINHA /C1 = COLUNA 1 / C2 = COLUNA2
+	public int[][][] geraMatriz(int l, int c1, int c2){ 
+		int w , x , y , a, b, c;
+		//matrix m = new matrix();
+		int matrix[][][] = new int [l][c1][c2];
+		for(w=0;w<matrix.length ;w++) {
+			c=geraNum(min, max);
+			//c = r.nextInt((9000 - 1000) + 1) + 1000;
+			matrix[w][0][0]=c;			
+			for(x=0;x<matrix[w].length;x++) {
+				b=geraNum(min, max);
+				//b = r.nextInt((9000 - 1000) + 1) + 1000;
+				matrix[w][x][0]=b;
+				for(y=0;y<matrix[w][x].length;y++) {
+					a=geraNum(min, max);
+					//a = r.nextInt((9000 - 1000) + 1) + 1000;
+					matrix[w][x][y]=a;
+				}				
+			}			
+		}
+		return matrix;		
+	}
+	
+	public void showMatrix(int[][][] matrix) {
+		int w , x , y;
+		
+		for(w=0;w<matrix.length ;w++) {							
+			for(x=0;x<matrix[0].length;x++) {				
+				for(y=0;y<matrix[0][0].length ;y++) {					
+					System.out.print(matrix[w][x][y] + " ");
+				}
+				
+				System.out.print("\t \t");
+			}	
+			
+			System.out.println("\t");
+		}
+	}
+	
 	
 	
 
